6330a71328a01cdf380ad724047c8341442f0210
Jack Gindi <jack.gindi@opengov.com>
2017-12-07 13:34:40 -0800

    allowed get and post access control methods

--- backend/src/server.py
+++ backend/src/server.py
@@ -11,7 +11,7 @@ import change_clicks as cc
 app = Flask(__name__)
 api = Api(app)
 
-access_control_response_headers = {'Access-Control-Allow-Origin': '*', 'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'}
+access_control_response_headers = {'Access-Control-Allow-Origin': '*', 'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept', 'Access-Control-Allow-Methods': 'GET, POST'}
 
 class GenerateChartRoute(Resource):
   def get(self):
